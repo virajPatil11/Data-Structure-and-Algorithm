cc568a74f6428f9ccde2c5d0bbf62c230d968244
Viraj Patil <69743653+virajPatil11@users.noreply.github.com>
2021-10-06 15:44:07 +0530

    Add files via upload

--- mathematics/Practice.java
+++ mathematics/Practice.java
@@ -1,23 +1,27 @@
 package Again;
 public class Practice {
 	public static void main(String[] args) {
-		int arr[]  = {0,1,0,0,3,12};
 		
-		int curr = 0;
-		for(int i = 0; i < arr.length; i++) {
-			if(arr[i] != 0) {
-				arr[curr] = arr[i];
-				curr++; 
-			}
-		}
-		for(int i = curr; i < arr.length; i++) {
-			arr[i] = 0;
+		
+		int arr[] = {1,1,2,3,3,4,5,5};
+		int n = arr.length;
+		n = remove(arr, n);
+		for(int i = 0; i < n; i++){
+			System.out.println(arr[i]);
 		}
 		
-		for(int i = 0;i < arr.length; i++) {
-			System.out.print(arr[i] + " ");
-		}
+	}
+	static int remove(int arr[],int n) {
+		if(n == 0 || n == 1)
+			return n;
 		
+		int j = 0;
+		for(int i = 0; i < arr.length - 1;i++) {
+			if(arr[i] != arr[i+1])
+				arr[j++] = arr[i];
+		}
+		arr[j++] = arr[n-1];
+		return j;
 	}
 	
 }
